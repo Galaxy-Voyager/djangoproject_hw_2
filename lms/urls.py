f1ecf516513e3a809e4071a614b069fcbcf8aee5
Damir <leheln16@gmail.com>
2026-01-24 17:33:38 +0500

    Switching to PostgreSQL and minor improvements

--- lms/urls.py
+++ lms/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView
 
-from .views import LessonListCreateView, LessonRetrieveUpdateDestroyView
+# Создаем router для приложения lms
+app_name = 'lms'
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
-    path("", LessonListCreateView.as_view(), name="lesson-list-create"),
-    path("<int:pk>/", LessonRetrieveUpdateDestroyView.as_view(), name="lesson-detail"),
+    path('', include(router.urls)),
+    path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
+    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),
 ]
